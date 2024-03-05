9d36a14cf2dc8579bb801e449c8611dc9b247ee2
fatemeh <sffekri@gmail.com>
2024-03-05 11:27:22 -0500

    Create script.js

---
+++ Script/script.js
@@ -0,0 +1,15 @@
+/* 
+
+Class: User Interface
+Assignment: Final Project
+
+Team 4
+    Craig Collins (1038789)
+    James Fallouh (6171620)
+    SeyedehFatemeh Fekribaygi (6284424)
+
+    Current Date: TBD
+    Filename: script.js
+
+    Purpose: This is the script.js code.
+*/
